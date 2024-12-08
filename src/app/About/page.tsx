4f0247414ef7a3595d0de9f20f9c0721ede03067
KanwalRafique <160254148+KanwalRafique@users.noreply.github.com>
2024-12-08 23:34:43 +0500

    update pages

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,9 +1,14 @@
 import Link from "next/link";
+import Navbar from "../Components/Navbar";
+
 
 export default function About() {
   return (
+    
     <div>
-        <h1>My name is kanwal </h1>
+      <Navbar />
+        <h1>My name is kanwal Sir due to too much shortage of time electricity and many other issues i have done this i will complete it and submit it again 
+          i worked whole night for this as i have not a strong IT backgroung so it is difficult to me but whatever i have done i'm uploading to show u </h1>
     </div>
   )
 }
--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import Navbar from "../Components/Navbar";
 
-export default function Pages() {
+
+export default function Contact() {
   return (
+    
     <div>
-        <Navbar />
-        <h1 className="font-extrabold size-10">My name is kanwal Sir due to too much shortage of time electricity and many other issues i have done this i will complete it and submit it again 
+      <Navbar />
+        <h1>My name is kanwal Sir due to too much shortage of time electricity and many other issues i have done this i will complete it and submit it again 
           i worked whole night for this as i have not a strong IT backgroung so it is difficult to me but whatever i have done i'm uploading to show u </h1>
     </div>
   )
