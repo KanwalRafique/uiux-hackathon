1bd6228a9790a37059cf91a4ededd9a42b7a639f
KanwalRafique <160254148+KanwalRafique@users.noreply.github.com>
2024-12-08 23:42:18 +0500

    update page.tsx

--- src/app/Pages/page.tsx
+++ src/app/Pages/page.tsx
@@ -5,7 +5,7 @@ export default function Pages() {
   return (
     <div>
         <Navbar />
-        <h1 className="font-extrabold size-10">My name is kanwal Sir due to too much shortage of time electricity and many other issues i have done this i will complete it and submit it again 
+        <h1 className="font-extrabold size-10 flex justify-center">My name is kanwal Sir due to too much shortage of time electricity and many other issues i have done this i will complete it and submit it again 
           i worked whole night for this as i have not a strong IT backgroung so it is difficult to me but whatever i have done i'm uploading to show u </h1>
     </div>
   )
